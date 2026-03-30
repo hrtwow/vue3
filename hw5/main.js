d5f4e648171adf1fe3a28c513f2967bec5c33cbd
Somi <hrtw222@gmail.com>
2026-03-30 23:03:05 +0800

    update

--- hw5/main.js
+++ hw5/main.js
@@ -22,7 +22,7 @@ const userModal = {
   data() {
     return {
       modal: null,
-      num: 0,          //這邊有沒有寫都沒差，因為 open() 會回到預設值
+      num: 0,          //這邊有沒有寫都沒差，因為 open() 會回到預設值1
     }
   },
   template: '#userProductModal',
@@ -58,18 +58,22 @@ const app = createApp({
       products: [],
       p:{},
       isLoading: false,
+      cartLoading: false,
       cart:{
         carts: [],
-
+        final_total: 0, //折扣價 final_total 是套完優惠券的價格
+        total: 0,       //總價
       },
       btnStatus:{
         addToCartLoading: '', //放該筆id
+        updateQtyLoading: '', //放該筆id
       },
+
     }
   },
   methods: {
     getProducts(page = 1) {
-      this.isLoading = true;
+      // this.isLoading = true;
 
       axios.get(`${site}/api/${apiPath}/products?page=${page}`)
         .then(res => {
@@ -83,7 +87,7 @@ const app = createApp({
 
         })
         .finally(() => {
-          this.isLoading = false;
+          // this.isLoading = false;
         })
 
     },
@@ -103,28 +107,93 @@ const app = createApp({
         }
       }
 
-      //局部loading
-      this.btnStatus.addToCartLoading = productId ; //等於該筆ID
+      //局部loading，先做避免重複呼叫API就好
+      this.btnStatus.addToCartLoading = productId ; //等於該筆ID, 避免連續按加入購物車, 其實也不會怎樣..
+      // this.cartLoading = true;                    //下方table要鎖住
 
       axios.post(`${site}/api/${apiPath}/cart`, param)
           .then(res =>{
             console.log(res);
             
             this.btnStatus.addToCartLoading = ''; //解除loading
-            
+            // this.cartLoading = false;
             this.$refs.pModal.close();
+
             //顯示購物車，呼叫API
-            // this.getCart()
+            this.getCart();
           })
-          
-      
     },
     getCart(){
       axios.get(`${site}/api/${apiPath}/cart`)
           .then(res =>{
-            const {data} = res.data;
-            this.cart.carts = data.carts;
+            this.cart = res.data.data;
+            console.log(this.cart);
+          })
+    },
+    /** 調整購物車數量 */
+    changeCartQty(item){
+      // console.log(item);//item.id是購物車id
+      
+      const param = {
+        data:{
+          product_id: item.product_id,
+          qty: item.qty
+        }
+      }
 
+      //局部loading
+      this.btnStatus.updateQtyLoading = item.id ; //等於該筆購物車ID, 避免連續按上下btn
+      // this.cartLoading = true;                    //下方table也要鎖住
+
+      axios.put(`${site}/api/${apiPath}/cart/${item.id}`, param)
+          .then(res =>{
+            console.log(res);
+            
+            alert(res.data.message)
+            this.btnStatus.updateQtyLoading = ''; //解除loading
+            // this.cartLoading = false;
+            //顯示購物車，呼叫API
+            this.getCart();
+          })
+    },
+    onQtyInput(e, item){  //其實實務上不建議使用input, 會用下拉選單, 避免檢核太難做
+      let val = Number(e.target.value);
+      
+      // 統一處理所有情況
+      if (!val || val < 1) val = 1; //空字串或0 => !val = true
+
+      item.qty = Math.floor(val);  //正小數只取整數 e.g., 2.2 => 2
+    },
+    remove(id){
+      const isConfirm = confirm('確定要刪除嗎？');
+      // console.log(isConfirm);
+      
+      if (!isConfirm) return; // 使用者按取消就直接結束
+
+      axios.delete(`${site}/api/${apiPath}/cart/${id}`)
+          .then(res =>{
+            console.log(res);
+            
+            alert(res.data.message)
+
+            //顯示購物車，呼叫API
+            this.getCart();
+          })
+    },
+    removeAll(){
+      const isConfirm = confirm('確定要全部刪除嗎？');
+      // console.log(isConfirm);
+      
+      if (!isConfirm) return; // 使用者按取消就直接結束
+
+      axios.delete(`${site}/api/${apiPath}/carts`)
+          .then(res =>{
+            console.log(res);
+            
+            alert(res.data.message)
+
+            //顯示購物車，呼叫API
+            this.getCart();
           })
     }
   },
